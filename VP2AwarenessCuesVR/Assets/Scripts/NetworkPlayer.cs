67475591b6c7adbf4a6e73bbb7a8d7fc4009b098
Mher Kondratyan <Mher.Kondratyan@gmx.de>
2021-07-09 13:54:51 +0200

    Networking bug found

--- VP2AwarenessCuesVR/Assets/Scripts/NetworkPlayer.cs
+++ VP2AwarenessCuesVR/Assets/Scripts/NetworkPlayer.cs
@@ -4,12 +4,14 @@ using MLAPI.Messaging;
 using MLAPI.NetworkVariable;
 using UnityEngine;
 
+
 namespace Network
 {
     public class NetworkPlayer : NetworkBehaviour
     {
+        //[SerializeField] public NetworkObject VRPlayerPrefab;
 
-        public GameObject observerPlayer;
+        public GameObject observerView;
         public NetworkVariableVector3 Position = new NetworkVariableVector3(new NetworkVariableSettings
         {
             WritePermission = NetworkVariablePermission.ServerOnly,
@@ -18,22 +20,42 @@ namespace Network
         
         public override void NetworkStart()
         {
-            InitiatePlayer();
+            InitializePlayer();
+
+            Move();
         }
 
-        public void InitiatePlayer() 
+        public void InitializePlayer() 
         {
             if (NetworkManager.Singleton.IsHost) {
                 Debug.Log("is Host and server");
-                
-                observerPlayer = GameObject.Find("Observer");
-                observerPlayer.SetActive(false);
+                Debug.Log("PlayerObject: " + NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject);
+                observerView = GameObject.Find("Observer");
+                observerView.SetActive(false);
+                //Destroy(NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject);
+                //netObject = GameObject.Find("Observer");
+                //NetworkObject netObject = GetComponent<NetworkObject>();
+                //netobject.NetworkHide();
+
+                //GameObject go = Instantiate(Observer, Vector3.zero, Quaternion.identity);
+                //go.GetComponent<NetworkObject>().Spawn();
+                /**
+                NetworkObject NewVRPlayer = Instantiate(VRPlayerPrefab, Vector3.zero, Quaternion.identity);
+                NewVRPlayer.SpawnAsPlayerObject(NetworkManager.Singleton.LocalClientId, null, false);
+                Debug.Log("PlayerObject: " + NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject);
+                */
             } 
             else 
             {
                 Debug.Log("is Client");
-                observerPlayer = GameObject.Find("Observer");
-                observerPlayer.SetActive(false);
+                Debug.Log(NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject);
+                //observerPlayer = GameObject.Find("Observer");
+                //observerPlayer.SetActive(true);
+
+
+
+                //clientPlayerObject = NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject;
+                //Destroy(NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject);
             }
         }
         
@@ -59,7 +81,7 @@ namespace Network
 
         static Vector3 GetRandomPositionOnPlane()
         {
-            return new Vector3(Random.Range(1f, 3f), 1f, Random.Range(1f, 3f));
+            return new Vector3(Random.Range(1f, 5f), 1f, Random.Range(1f, 5f));
         }
         
         void Update()
